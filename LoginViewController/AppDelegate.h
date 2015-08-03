2ed05c8ade8eb8603c4d0894805a9a61e9533391
= <=>
2015-08-03 20:55:53 +0800

    LoginViewController

---
+++ LoginViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginViewController
+//
+//  Created by Mac on 15/8/3.
+//  Copyright (c) 2015年 hansonboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
